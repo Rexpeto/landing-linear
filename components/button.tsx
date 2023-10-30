f5315e51270acf3513f75fa8fc211ce07bf35b36
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-10-30 17:04:19 -0400

    cva button

--- components/button.tsx
+++ components/button.tsx
@@ -1,12 +1,35 @@
 import Link from "next/link";
+import { cva, VariantProps } from "class-variance-authority";
 
-interface Props {
+interface Props extends VariantProps<typeof buttonClasses> {
   children: React.ReactNode;
   href: string;
 }
 
-const Button = ({ children, href }: Props) => {
-  return <Link href={href}>{children}</Link>;
+const buttonClasses = cva("inline-flex rounded-full text-white", {
+  variants: {
+    variants: {
+      primary: "bg-btn-primary hover:text-shadow hover:box-shadow",
+      gradient: "bg-primary-gradient",
+    },
+    size: {
+      small: "text-base px-3 py-1.5",
+      medium: "text-sm px-4 py-1.5",
+      large: "text-lg px-6 py-2",
+    },
+    defaultVariants: {
+      variants: "primary",
+      size: "medium",
+    },
+  },
+});
+
+const Button = ({ children, href, variants, size }: Props) => {
+  return (
+    <Link className={buttonClasses({ variants, size })} href={href}>
+      {children}
+    </Link>
+  );
 };
 
 export default Button;
