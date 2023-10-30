0833ee228e0f331c0b86cc759cc5451f3b3a0f18
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-10-30 17:04:37 -0400

    header

--- components/header.tsx
+++ components/header.tsx
@@ -1,19 +1,19 @@
 import Image from "next/image";
 import Link from "next/link";
-import { Container } from ".";
+import { Button, Container } from ".";
 import { Links } from "@/models";
 
 const Header = () => {
   return (
-    <header className="fixed top-0 left-0 w-full border-b border-white/10">
+    <header className="fixed top-0 left-0 w-full border-b border-white/10 backdrop-blur-md">
       <Container className="flex items-center justify-between h-[var(--navigation-height)]">
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-6">
           <Link href="/" className="flex items-center text-md gap-2">
             <Image src="/logo.svg" alt="Vypers" width={28} height={28} />
             <span className="font-semibold">Vypers</span>
           </Link>
 
-          <nav className="flex items-center gap-4 h-full">
+          <nav className="flex items-center gap-6 h-full">
             {Links.map((link) => (
               <Link
                 className="text-sm hover:text-gray-400 first-letter:uppercase transition duration-150"
@@ -33,12 +33,9 @@ const Header = () => {
           >
             Log in
           </Link>
-          <Link
-            href="#"
-            className="flex items-center text-sm py-2 px-4 rounded-full bg-btn-primary text-white"
-          >
+          <Button href="#" variants="primary" size="medium">
             Sign up
-          </Link>
+          </Button>
         </div>
       </Container>
     </header>
