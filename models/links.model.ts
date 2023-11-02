808e05ade7802925e5202994aa3135eedd1b3a26
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-11-02 15:24:47 -0400

    footer links

--- models/links.model.ts
+++ models/links.model.ts
@@ -32,3 +32,50 @@ export const Links = [
     url: "#",
   },
 ];
+
+export const footerLinks = [
+  {
+    title: "Product",
+    links: [
+      { name: "Features", url: "#" },
+      { name: "Pricing", url: "#" },
+      { name: "Marketplace", url: "#" },
+      { name: "Integrations", url: "#" },
+      { name: "Changelogs", url: "#" },
+      { name: "Docs", url: "#" },
+      { name: "Download", url: "#" },
+    ],
+  },
+
+  {
+    title: "Company",
+    links: [
+      { name: "About", url: "#" },
+      { name: "Blog", url: "#" },
+      { name: "Careers", url: "#" },
+      { name: "customers", url: "#" },
+      { name: "Brand", url: "#" },
+    ],
+  },
+
+  {
+    title: "Resources",
+    links: [
+      { name: "Community", url: "#" },
+      { name: "Contact", url: "#" },
+      { name: "DPA", url: "#" },
+      { name: "Privacy Policy", url: "#" },
+      { name: "Terms of Service", url: "#" },
+      { name: "Report a vulnerability", url: "#" },
+    ],
+  },
+
+  {
+    title: "Developers",
+    links: [
+      { name: "API", url: "#" },
+      { name: "Status", url: "#" },
+      { name: "GitHub", url: "#" },
+    ],
+  },
+];
