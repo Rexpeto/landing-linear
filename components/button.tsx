03f4e274d19cc146f72714b586b204468a1631cf
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-11-03 16:46:27 -0400

    varient secundary

--- components/button.tsx
+++ components/button.tsx
@@ -6,23 +6,28 @@ interface Props extends VariantProps<typeof buttonClasses> {
   href: string;
 }
 
-const buttonClasses = cva("inline-flex rounded-full text-white", {
-  variants: {
+const buttonClasses = cva(
+  "inline-flex rounded-full text-white transition duration-150",
+  {
     variants: {
-      primary: "bg-btn-primary hover:text-shadow hover:box-shadow",
-      gradient: "bg-primary-gradient",
-    },
-    size: {
-      small: "text-base px-3 py-1.5",
-      medium: "text-sm px-4 py-1.5",
-      large: "text-lg px-6 py-2",
-    },
-    defaultVariants: {
-      variants: "primary",
-      size: "medium",
+      variants: {
+        primary: "bg-btn-primary hover:text-shadow hover:box-shadow",
+        secondary:
+          "flex items-center gap-2 text-off-white bg-white bg-opacity-10 hover:bg-opacity-20 border-border-white backdrop-filter-[12px]",
+        gradient: "bg-primary-gradient",
+      },
+      size: {
+        small: "text-xs px-3 py-1.5",
+        medium: "text-sm px-4 py-1.5",
+        large: "text-lg px-6 py-2",
+      },
+      defaultVariants: {
+        variants: "primary",
+        size: "medium",
+      },
     },
   },
-});
+);
 
 const Button = ({ children, href, variants, size }: Props) => {
   return (
