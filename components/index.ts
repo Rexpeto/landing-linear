aa1d28e75486116900b8d8103066db5fa193abf5
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-11-03 16:32:58 -0400

    update barrel

--- components/index.ts
+++ components/index.ts
@@ -4,3 +4,4 @@ export * from "./hero";
 export { default as Header } from "./header";
 export { default as Footer } from "./footer";
 export { default as Button } from "./button";
+export { default as GetIcon } from "./getIcon";
