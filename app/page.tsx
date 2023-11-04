1a7fd45f4ddfd8245e7b6491d19dd7f2bf82d397
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-11-04 13:51:01 -0400

    add animation fade in

--- app/page.tsx
+++ app/page.tsx
@@ -12,20 +12,30 @@ const Home = () => {
   return (
     <Container className="pt-[6.2rem]">
       <Hero>
-        <Button href="/" variants="secondary" size="small">
+        <Button
+          href="/"
+          variants="secondary"
+          size="small"
+          className="animate-fade-in"
+        >
           Introducing Linear Ask
           <GetIcon icon="IoIosArrowForward" />
         </Button>
-        <HeroTitle>
+        <HeroTitle className="animate-fade-in [--animation-timing:200ms] opacity-0 translate-y-[-1rem]">
           Linear is a better way <br className="hidden lg:block" /> to build
           products
         </HeroTitle>
-        <HeroSubtitle>
+        <HeroSubtitle className="animate-fade-in [--animation-timing:400ms] translate-y-[-1rem] opacity-0">
           Meet the new standard for modern software development.
           <br className="hidden lg:block" /> Streamline issues, sprints, and
           product roadmaps.
         </HeroSubtitle>
-        <Button href="/" variants="primary" size="large">
+        <Button
+          href="/"
+          variants="primary"
+          size="large"
+          className="animate-fade-in [--animation-timing:600ms] translate-y-[-1rem] opacity-0"
+        >
           Get Started <GetIcon icon="IoIosArrowForward" />
         </Button>
         <Image
