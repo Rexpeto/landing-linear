74e07757cf63c9e5de8c68567b09f8cfe02e23d7
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-11-06 20:03:59 -0400

    lines effect random

--- components/hero-images.tsx
+++ components/hero-images.tsx
@@ -1,10 +1,58 @@
 "use client";
 import classNames from "classnames";
 import Image from "next/image";
+import { CSSProperties, useEffect, useRef, useState } from "react";
 import { useInView } from "react-intersection-observer";
 
+interface Lines {
+  id: number;
+  direction: "to left" | "to top";
+  duration: number;
+  size: number;
+}
+
+// Generate random number
+const randomNumber = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+// Component
 const HeroImages = () => {
   const { ref, inView } = useInView({ threshold: 0.4, triggerOnce: true });
+
+  const [lines, setLines] = useState<Lines[]>([]);
+
+  const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+
+  const removeLines = (id: number) => {
+    setLines((lines) => lines.filter((line) => line.id !== id));
+  };
+
+  useEffect(() => {
+    if (!inView) return;
+
+    const renderLines = (timeout: number) => {
+      timeoutRef.current = setTimeout(() => {
+        setLines((lines) => [
+          ...lines,
+          {
+            id: Math.random(),
+            direction: randomNumber(0, 1) ? "to left" : "to top",
+            duration: randomNumber(1300, 3500),
+            size: randomNumber(10, 38),
+          },
+        ]);
+      }, timeout);
+    };
+
+    renderLines(randomNumber(800, 2500));
+
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    };
+  }, [inView, lines]);
 
   return (
     <div className="[perspective:2000px] mt-48" ref={ref}>
@@ -16,6 +64,29 @@ const HeroImages = () => {
           inView && "before:animate-image-glow",
         )}
       >
+        <div className="absolute w-full h-full top-0 left-0 z-20">
+          {lines.map((line, index) => (
+            <span
+              key={index}
+              onAnimationEnd={() => removeLines(line.id)}
+              style={
+                {
+                  "--direction": line.direction,
+                  "--size": line.size,
+                  "--animation-duration": `${line.duration}ms`,
+                } as CSSProperties
+              }
+              className={classNames(
+                "absolute top-0 block h-[1px] w-[10rem] bg-glow-lines",
+                line.direction === "to left" &&
+                  `left-0 h-[1px] w-[calc(var(--size)*0.5rem)] animate-glow-line-horizontal md:w-[calc(var(--size)*1rem)]`,
+                line.direction === "to top" &&
+                  `right-0 h-[calc(var(--size)*0.5rem)] w-[1px] animate-glow-line-vertical md:h-[calc(var(--size)*1rem)]`,
+              )}
+            />
+          ))}
+        </div>
+
         <svg
           className={classNames(
             "absolute left-0 top-0 h-full w-full",
