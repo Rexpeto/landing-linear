12d2fa343cdc44ae5f60dee94018e2d49a56f532
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-11-08 18:18:51 -0400

    section

---
+++ app/(root)/sections/clientLogo/clientLogo.tsx
@@ -0,0 +1,11 @@
+import { Clients } from "./components";
+
+const SectionClientLogo = () => {
+  return (
+    <section className="py-12">
+      <Clients />
+    </section>
+  );
+};
+
+export default SectionClientLogo;
