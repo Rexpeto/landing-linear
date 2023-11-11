47386795d3e6628b118015289ed98d284bb27c19
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-11-11 15:30:37 -0400

    responsive and title

--- app/(root)/sections/unlike-tool/sectionUnlike.tsx
+++ app/(root)/sections/unlike-tool/sectionUnlike.tsx
@@ -10,8 +10,18 @@ const SectionUnlike = () => {
   return (
     <section>
       <Container>
-        <div className="text-white">
-          <div className="flex flex-wrap gap-6">
+        <div className="text-white text-center">
+          <h2 className="mb-4 text-2xl md:mb-2 md:text-5xl">
+            Unlike any tool <br className="hidden md:block" /> you’ve used
+            before
+          </h2>
+          <p className="mx-auto mb-12 max-w-[68rem] text-lg text-primary-text">
+            Designed to the last pixel and engineered with unforgiving
+            precision, Linear combines UI elegance with world-class performance.
+          </p>
+        </div>
+        <div className="h-[48rem] overflow-hidden md:h-auto">
+          <div className="flex snap-x snap-mandatory md:flex-wrap overflow-x-auto gap-6">
             <ShortcutsKeys />
 
             <Breathtakingly />
