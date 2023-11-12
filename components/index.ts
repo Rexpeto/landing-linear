2ee977ff936415d40fc1dc38cfba0cca14b80e1f
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-11-12 09:14:11 -0400

    update

--- components/index.ts
+++ components/index.ts
@@ -2,4 +2,5 @@ export { default as Container } from "./container";
 export { default as Header } from "./header";
 export { default as Footer } from "./footer";
 export { default as Button } from "./button";
+export * from "./button";
 export { default as GetIcon } from "./getIcon";
