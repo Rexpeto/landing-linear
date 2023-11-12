d8b5db4a8b8b2c52255427116fab71640bb9e425
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-11-12 15:08:22 -0400

    resize

---
+++ app/(root)/sections/unlike-tool/components/command-line.tsx
@@ -0,0 +1,13 @@
+const CommandLine = () => {
+  return (
+    <div className="bg-glass-gradient snap-center shrink-0 min-h-[48rem] text-center w-full md:basis-[calc(66.66%-12px)] md:max-w-[calc(66.66%-12px)] rounded-[4.8rem] p-8 md:p-14 border-transparent-white border">
+      <p className="text-lg md:text-2xl mb-4">Meet your command line</p>
+
+      <p className="text-primary-text text-sm md:text-md">
+        Complete any action in seconds with the global command menu.
+      </p>
+    </div>
+  );
+};
+
+export default CommandLine;
