40472e902f80f751fd056ebea9fc101e2d7ecf04
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-11-14 19:20:35 -0400

    hightlighted keys

--- app/(root)/sections/unlike-tool/components/shortcutsKeys.tsx
+++ app/(root)/sections/unlike-tool/components/shortcutsKeys.tsx
@@ -1,26 +1,28 @@
 "use client";
+
 import { Button, Highlight } from "@/components";
 import { KeyboardIllustration } from ".";
 import { MouseEvent, useRef } from "react";
 
 const shortcuts = [
   { text: "Opens command line", key: "⌘K" },
-  { text: "Assign issue to me", key: "I" },
-  { text: "Assign issue to", key: "A" },
-  { text: "Change issue status", key: "S" },
-  { text: "Set issue priority", key: "P" },
-  { text: "Add issue label", key: "L" },
-  { text: "Set due date", key: "⇪D" },
-  { text: "Set parent issue", key: "⇪⌘P" },
-  { text: "Create new issue", key: "C" },
-  { text: "Create new issue from template", key: "⌥C" },
-  { text: "Move to project", key: "⌥P" },
+  { text: "Assign issue to me", key: "i" },
+  { text: "Assign issue to", key: "a" },
+  { text: "Change issue status", key: "s" },
+  { text: "Set issue priority", key: "p" },
+  { text: "Add issue label", key: "l" },
+  { text: "Set due date", key: "⇪d" },
+  { text: "Set parent issue", key: "⇪⌘p" },
+  { text: "Create new issue", key: "c" },
+  { text: "Create new issue from template", key: "⌥c" },
+  { text: "Move to project", key: "⌥p" },
 ];
 
 const ShortcutsKeys = () => {
   const wrapperRef = useRef<HTMLDivElement>(null);
+  const illustrationWrapperRef = useRef<HTMLDivElement>(null);
 
-  const onShortcutClick = (ev: MouseEvent<HTMLButtonElement>) => {
+  const onShortcutClick = (ev: MouseEvent<HTMLButtonElement>, key: string) => {
     ev.preventDefault();
 
     if (!wrapperRef.current) return;
@@ -29,12 +31,34 @@ const ShortcutsKeys = () => {
       left: ev.currentTarget.offsetLeft - wrapperRef.current.clientWidth / 2,
       behavior: "smooth",
     });
+
+    if (!illustrationWrapperRef.current) return;
+
+    const keyArray = key.split("");
+
+    const keyElement = keyArray.map(
+      (key) =>
+        illustrationWrapperRef.current?.querySelector(`[data-key="${key}"]`),
+    );
+
+    illustrationWrapperRef.current
+      .querySelectorAll(".active")
+      .forEach((element) => element.classList.remove("active"));
+
+    keyElement.forEach((element) => {
+      if (!element) return;
+
+      element.classList.add("active");
+    });
   };
 
   return (
     <div className="shrink-0 snap-center flex flex-col bg-glass-gradient min-h-[46rem] text-center w-full md:basis-[calc(66.66%-12px)]  md:max-w-[calc(66.66%-12px)] rounded-[4.8rem] p-8 md:p-14 border-transparent-white border">
       <div className="flex flex-col gap-4 mb-4">
-        <div className="mask-keyboard h-full w-full">
+        <div
+          ref={illustrationWrapperRef}
+          className="mask-keyboard h-full w-full"
+        >
           <KeyboardIllustration />
         </div>
 
@@ -47,11 +71,11 @@ const ShortcutsKeys = () => {
               <Button
                 key={shortcut.key}
                 className="shrink-0 mb-2 snap-center last:mr-[50vw] first:ml-[50vw]"
-                onClick={() => {}}
+                onClick={(ev) => onShortcutClick(ev, shortcut.key)}
                 variants="secondary"
                 size="medium"
               >
-                <Highlight>{shortcut.key}</Highlight>
+                <Highlight className="uppercase">{shortcut.key}</Highlight>
                 {shortcut.text}
               </Button>
             ))}
