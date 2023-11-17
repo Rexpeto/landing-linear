73adbe323203e3a0f94c67a3690bd24e8d4032ce
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-11-17 16:14:18 -0400

    command menu animation

--- app/(root)/sections/unlike-tool/components/commandMenu.tsx
+++ app/(root)/sections/unlike-tool/components/commandMenu.tsx
@@ -1,6 +1,38 @@
+"use client";
+
+import { commandOptions } from "@/models/commandOptions.model";
+import classNames from "classnames";
+import { useEffect, useRef, useState } from "react";
+
 const CommandMenu = () => {
+  const [opened, setOpened] = useState(false);
+  const commandMenuRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const toggleCommandMenu = (e: MouseEvent) => {
+      const clickedOutside = !commandMenuRef.current?.contains(
+        e.target as Node,
+      );
+      setOpened(clickedOutside ? false : true);
+    };
+
+    window.addEventListener("click", toggleCommandMenu);
+
+    return () => {
+      window.removeEventListener("click", toggleCommandMenu);
+    };
+  }, []);
+
   return (
-    <div className="absolute bg-transparent-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 mt-10 flex flex-col items-start rounded-xl w-[90vw] max-w-[64rem] border border-transparent-white">
+    <div
+      ref={commandMenuRef}
+      className={classNames(
+        "absolute left-[calc(50%+7.5rem)] flex w-[90vw] max-w-[64rem] -translate-x-1/2 flex-col items-start rounded-xl border border-transparent-white bg-transparent-white shadow-[rgb(0_0_0_/_35%)_0px_7px_32px] transition-[transform,opacity] md:left-1/2",
+        opened &&
+          "translate-y-[12.8rem] opacity-100 md:translate-y-[2.4rem] opened",
+        !opened && "translate-y-[12.8rem] opacity-60",
+      )}
+    >
       <span className="bg-white/5 mt-3 mx-4 rounded-lg text-white/50 text-[12px] px-2 leading-10">
         LIN-111 Welkway lightning
       </span>
@@ -9,15 +41,15 @@ const CommandMenu = () => {
         placeholder="Type a command or search..."
       />
       <div className="flex flex-col w-full text-sm text-gray-400/90 transition duration-150">
-        <button className="py-3 px-4 text-start hover:bg-white/5 w-full">
-          Assign to...
-        </button>
-        <button className="py-3 px-4 text-start hover:bg-white/5 w-full">
-          Assign to...
-        </button>
-        <button className="py-3 px-4 text-start hover:bg-white/5 w-full">
-          Assign to...
-        </button>
+        {commandOptions.map(({ label, icon: Icon }) => (
+          <button
+            key={label}
+            className="flex items-center gap-3 py-3 px-4 text-start hover:bg-white/5 w-full"
+          >
+            <Icon />
+            <span>{label}</span>
+          </button>
+        ))}
       </div>
     </div>
   );
