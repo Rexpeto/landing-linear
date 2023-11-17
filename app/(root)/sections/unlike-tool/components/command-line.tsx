37cedd14a8c5af293ea3903d223c9a34d1c495eb
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-11-17 16:15:14 -0400

    resize and animation

--- app/(root)/sections/unlike-tool/components/command-line.tsx
+++ app/(root)/sections/unlike-tool/components/command-line.tsx
@@ -2,14 +2,16 @@ import { CommandMenu } from ".";
 
 const CommandLine = () => {
   return (
-    <div className="relative bg-glass-gradient snap-center shrink-0 min-h-[48rem] text-center items-center w-full md:basis-[calc(66.66%-12px)] md:max-w-[calc(66.66%-12px)] rounded-[4.8rem] p-8 md:p-14 border-transparent-white border">
-      <p className="text-lg md:text-2xl mb-4">Meet your command line</p>
-
-      <p className="text-primary-text text-sm md:text-md">
-        Complete any action in seconds with the global command menu.
-      </p>
-
+    <div className="relative flex min-h-[45rem] w-full shrink-0 snap-center flex-col items-center justify-start overflow-hidden rounded-[4.8rem] border border-transparent-white bg-glass-gradient p-8 text-center md:max-w-[calc(66.66%-12px)] md:basis-[calc(66.66%-12px)] md:py-12">
       <CommandMenu />
+
+      <div className="[.opened+&]:opacity-0 transition">
+        <p className="text-lg md:text-2xl mb-4">Meet your command line</p>
+
+        <p className="text-primary-text text-sm md:text-md">
+          Complete any action in seconds with the global command menu.
+        </p>
+      </div>
     </div>
   );
 };
