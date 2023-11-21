8f5d8480a7d4e4bdd425a2b04288028264437a47
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-11-21 14:17:48 -0400

    section initial

---
+++ app/(root)/sections/enjoy-issue-tracking/enjoy-issue-tracking.tsx
@@ -0,0 +1,13 @@
+import { Features } from "./components";
+
+const EnjoyIssueTracking = () => {
+  return (
+    <Features title="Issue Tracking you'll enjoy using">
+      <Features.Main />
+      <Features.Grid />
+      <Features.Cards />
+    </Features>
+  );
+};
+
+export default EnjoyIssueTracking;
