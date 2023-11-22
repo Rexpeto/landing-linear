0dd360b7a6cb196daa4734f60aed96701c9e5749
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-11-22 13:33:44 -0400

    barrel

---
+++ app/(root)/sections/enjoy-issue-tracking/icons/index.ts
@@ -0,0 +1,6 @@
+export { default as AutomatedBacklogIcon } from "./automated-backlog";
+export { default as CustomViewsIcon } from "./custom-views";
+export { default as DiscussionIcon } from "./discussion";
+export { default as IssuesIcon } from "./issues";
+export { default as ParentSubIcon } from "./parent-sub";
+export { default as WorkflowsIcon } from "./workflows";
