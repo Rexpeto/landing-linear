ea5dc29782721080c8c13c8963bbadd5fd161a3a
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-11-26 13:43:11 -0400

    model feature card

--- models/features.models.ts
+++ models/features.models.ts
@@ -33,6 +33,7 @@ export type FeatureCardProps = {
     title: string;
     text: string;
     image: string;
+    imageClassName: string;
   }[];
 };
 
@@ -74,10 +75,12 @@ export const FeaturesDataCards = [
     title: "List and board",
     text: "Switch between list and board layout to view work from any angle.",
     image: "/card-board.webp",
+    imageClassName: "max-w-none top-[30%] w-[200%]",
   },
   {
     title: "Make it yours",
     text: "Quickly apply filters and operators to refine your issue lists and create custom views.",
     image: "/card-views.webp",
+    imageClassName: "top-[30%] left-[24px] w-[110%]",
   },
 ];
