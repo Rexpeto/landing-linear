2b03bf85eb3739109b8bb39171309fbf72a40178
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-11-29 17:46:24 -0400

    update barrel

--- app/(root)/sections/enjoy-issue-tracking/components/index.ts
+++ app/(root)/sections/enjoy-issue-tracking/components/index.ts
@@ -1,1 +1,4 @@
 export * from "./Features";
+export { default as FeatureMain } from "./FeatureMain";
+export { default as FeatureCards } from "./FeatureCards";
+export { default as FeatureGrid } from "./FeatureGrid";
