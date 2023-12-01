86fc013fc7a2fd22d827c1bd3c941c7416c85c9d
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-12-01 15:05:14 -0400

    update barrel

--- components/index.ts
+++ components/index.ts
@@ -4,3 +4,7 @@ export { default as Footer } from "./footer";
 export { default as Button } from "./button";
 export * from "./button";
 export { default as GetIcon } from "./getIcon";
+export { default as Feature } from "./features";
+export { default as FeatureMain } from "./featureMain";
+export { default as FeatureGrid } from "./featureGrid";
+export { default as FeatureCards } from "./featureCards";
