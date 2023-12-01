e24d1d2186ae70107718db98b08f719af02b2afa
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-12-01 15:05:55 -0400

    delete

--- app/(root)/sections/enjoy-issue-tracking/components/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./Features";
-export { default as FeatureMain } from "./FeatureMain";
-export { default as FeatureCards } from "./FeatureCards";
-export { default as FeatureGrid } from "./FeatureGrid";
