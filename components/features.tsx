f8b9cf28ccd548dd48713c155cb2b59cf6d0f895
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-12-01 15:08:22 -0400

    features components

---
+++ components/features.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { Container } from "@/components";
+import { FeaturesProps } from "@/models";
+import classNames from "classnames";
+import { useInView } from "react-intersection-observer";
+
+const Features = ({ children, color, colorDark }: FeaturesProps) => {
+  const { ref, inView } = useInView({
+    threshold: 0.2,
+    triggerOnce: false,
+  });
+
+  return (
+    <div
+      ref={ref}
+      style={
+        {
+          "--feature-color": color,
+          "--feature-color-dark": colorDark,
+        } as React.CSSProperties
+      }
+      className={classNames(
+        "after:bg-[radial-gradient(ellipse_100%_40%_at_50%_60%,rgba(var(--feature-color),0.1),transparent) relative flex flex-col items-center overflow-x-clip before:pointer-events-none before:absolute before:h-[40rem] before:w-full before:bg-[conic-gradient(from_90deg_at_80%_50%,#000212,rgb(var(--feature-color-dark))),conic-gradient(from_270deg_at_20%_50%,rgb(var(--feature-color-dark)),#000212)] before:bg-no-repeat before:transition-[transform,opacity] before:duration-1000 before:ease-in before:[mask:radial-gradient(100%_50%_at_center_center,_black,_transparent)] before:[background-size:50%_100%,50%_100%] before:[background-position:1%_0%,99%_0%] after:pointer-events-none after:absolute after:inset-0",
+        inView &&
+          "is-visible before:opacity-100 before:[transform:rotate(180deg)_scale(2)]",
+        !inView && "before:opacity-40 before:rotate-180",
+      )}
+    >
+      <Container className="my-[23.2rem] w-full">{children}</Container>
+    </div>
+  );
+};
+
+export default Features;
